3f7f80785e300f5c1cb00378dcc906d6dcf2c9fa
santacoll19 <collazokel19@gmail.com>
2024-01-24 16:01:02 -0400

    complete?

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,32 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-
-request(url, (error, response, body) => {
+request(process.argv[2], (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     return;
   }
 
-  const tasks = JSON.parse(body);
-  const completedTasks = {};
-
-  tasks.forEach(task => {
+  const users = {};
+  for (const task of JSON.parse(body)) {
     if (task.completed) {
-      if (completedTasks[task.userId]) {
-        completedTasks[task.userId]++;
+      if (users[task.userId]) {
+        users[task.userId]++;
       } else {
-        completedTasks[task.userId] = 1;
+        users[task.userId] = 1;
       }
     }
-  });
-
-  let output = '{';
-  for (const userId in completedTasks) {
-    if (completedTasks[userId] > 0) {
-      output += `  '${userId}': ${completedTasks[userId]},\n`;
-    }
   }
-  output = output.slice(0, output.length - 2) + ' }';
-  console.log(output);
+  console.log(users);
 });
