390bc84e00447da824572889b893c33ff568c55e
santacoll19 <collazokel19@gmail.com>
2024-02-04 20:19:50 -0400

    test

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -2,33 +2,33 @@ const fs = require('fs');
 
 function readDatabase (path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(Error(err));
-        return;
-      }
-      const content = data.toString().split('\n');
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+    reject(Error(err));
+    return;
+    }
+    const content = data.toString().split('\n');
 
-      let students = content.filter((item) => item);
+    let students = content.filter((item) => item);
 
-      students = students.map((item) => item.split(','));
+    students = students.map((item) => item.split(','));
 
-      const fields = {};
-      for (const i in students) {
+    const fields = {};
+    for (const i in students) {
         if (i !== 0) {
-          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+        if (!fields[students[i][3]]) fields[students[i][3]] = [];
 
-          fields[students[i][3]].push(students[i][0]);
-        }
-      }
+        fields[students[i][3]].push(students[i][0]);
+    }
+    }
 
-      delete fields.field;
+    delete fields.field;
 
-      resolve(fields);
+    resolve(fields);
 
       //   return fields;
     });
-  });
+});
 }
 
 export default readDatabase;
