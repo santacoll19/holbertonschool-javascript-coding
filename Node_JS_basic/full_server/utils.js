267fa2c24df91bc63664d473c671eb5c182e3eba
santacoll19 <collazokel19@gmail.com>
2024-02-06 12:13:38 -0400

    utils lint

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function readDatabase(path) {
   return new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf8', (err, data) => {
+    fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
     reject(Error(err));
     return;
@@ -15,18 +15,16 @@ function readDatabase(path) {
 
     const fields = {};
     for (const i in students) {
-        if (i !== 0) {
+      if (i !== 0) {
         if (!fields[students[i][3]]) fields[students[i][3]] = [];
-
         fields[students[i][3]].push(students[i][0]);
-    }
+      }
     }
 
     delete fields.field;
 
     resolve(fields);
-
-      //   return fields;
+    // return fields;
     });
 });
 }
